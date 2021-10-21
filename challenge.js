6897c7f0c92faf6caef3762016d1df0b825428bc
Hector <vv96rca@gmail.com>
2021-10-21 12:54:00 +0530

    mob session

--- challenge.js
+++ challenge.js
@@ -1,26 +1,30 @@
+
 class Stack {
-constructor(){
-  this.size = 0
-  this.stack =[]
+  constructor(){
+    this.size = 0
+    this.stack=[]
+  }
+
+  push(val){
+    this.stack =[...this.stack,val]
+    this.size += 1
+  }
+
+  pop(){
+    this.stack.splice(this.size-1,1)
+    this.size -=1
+  }
+  min(){
+    let position = this.stack[0]
+    for(let i=0; i<this.size;i+=1)
+      position = Math.min(position,this.stack[i])
+
+      return position
+  }
 }
 
-  push(number) {
-    this.stack = [...this.stack,number]
-    this.size +=1
-  }
-  
-  pop() {
-    this.stack.splice(this.size -1 ,1);
-    this.size -= 1;
-  }
-  
-  min() {
-    let value = this.stack[0];
-    for(let i = 0; i<this.size;i+=1)
-      value = Math.min(value,this.stack[i])
-    return value
-  }
-}
+
+
 
 const stack = new Stack()
 stack.push(3)
